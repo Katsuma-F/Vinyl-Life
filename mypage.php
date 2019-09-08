9844403cc9fdfd807acd5d961e1abbe053fd6b8a
Katsuma-F <k.fujita.indigo1003@gmail.com>
2019-09-09 00:23:15 +0900

    Add post_UI in mypage.php

--- mypage.php
+++ mypage.php
@@ -10,6 +10,8 @@
 
   <link rel="stylesheet" href="./css/styles.css">
   <title>Vinyl-Life</title>
+  <link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="./folder/all.css">
 </head>
 
 <body>
@@ -21,16 +23,221 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="mainMenu">
-        <ul class="navbar-nav"></ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-menu" href="">マイページ</a>
+          </li>
+        </ul>
         <ul class="navbar-nav ml-auto text-dark">
           <form class="form-inline ml-auto">
-            <a href="" class="btn btn-secondary">ログイン</a>
-            <a href="" class="btn btn-danger">新規登録</a>
+            <div class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-dark" href="#" id="userMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="" class="rounded" style="width: 30px; height: 30px; margin-right: 10px"><span>名前</span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userMenu">
+                <a class="dropdown-item" href="">マイページ</a>
+                <a class="dropdown-item" href="">ログアウト</a>
+              </div>
           </form>
         </ul>
       </div>
     </div>
   </nav>
+
+ <!-- Main -->
+  <div class="container">
+    <div class="user-area">
+      <div class="user-thum">
+        <img src="" class="rounded-circle" alt="画像" style="width: 70px; height: 70px;">
+      </div>
+      <div class="user-username">
+        ユーザーネーム
+      </div>
+      <a href="#" target="_blank" class="user-sns">
+        <i class="fab fa-twitter" style="margin-right: 3px"></i>
+        @sns_acount
+      </a>
+      <p class="user-status">いいね獲得数: 0</p>
+      <p class="user-"></p>
+      <h6 class="user-description">自己紹介文。自己紹介文。自己紹介文。自己紹介文。</h6>
+    </div>
+    <div class="alert alert-danger alert-dismissble fade show" role="alert">
+      リストがまだありません。作成ボタンからセットを作成してみましょう！
+      <button type="button" class="close" data-dismiss="alert" aria-label="閉じる">
+        <span aria-hidden="true">×</span>
+      </button>
+    </div>
+    <center>
+      <a href="#" class="btn btn-danger col-12 mb-4 mx-auto" data-toggle="modal" data-target="#myModal">セットを作成する</a>
+    </center>
+
+    <!-- リスト -->
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+        <div class="card">
+          <a href="#">
+            <div class="row no-gutters card-area"></div>
+          </a>
+            <div class="title-area">
+              <a href="#" class="title">タイトル1</a>
+            </div>
+            <div class="profile-area">
+              <div class="profile-thum">
+                <a href="#">
+                  <img src="" class="rounded-circle" alt="画像">
+                </a>
+              </div>
+              <div class="profile-username">
+                <a href="#" class="profile-username">ユーザーネーム</a>
+              </div>
+            </div>
+            <div class="description-area">
+              <p class="description">セットの説明。セットの説明。セットの説明。</p>
+            </div>
+            <div class="info-area">
+              <i class="far fa-bookmark"></i>
+              <span class="num">0</span>
+              <i class="fas fa-heart"></i>
+              <span class="num">0</span>
+              <a href="#" class="text-primary" data-toggle="modal" data-target="#myEditModal58" style="margin-right: 7px;">
+                <i class="fas fa-pen" style="margin-right: 7px;"></i>
+                編集
+              </a>
+              <a href="#" class="text-primary" data-toggle="modal" data-target="#myDeleteModal58" >
+                <i class="fas fa-trash-alt" style="margin-right: 4px;"></i>
+                削除
+              </a>
+            </div>
+        </div>
+
+        <!-- 編集モーダルの設定 -->
+        <div class="modal fade" id="myEditModal58" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">セットの編集</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <form method="post">
+                <div class="modal-body">
+                  <input type="hidden" name="" value="">
+                  <p>
+                    セットの写真
+                    <input type="file" name="image" value="test" size="50" id="id_file" required>
+                  </p>
+                  <p>
+                    セット名
+                    <input type="text" name="title" class="form-control" value="" placeholder="セットのタイトル" maxlength="50" id="id_title" required>
+                  </p>
+                  <p>
+                    セットの説明
+                    <textarea name="description" rows="3" cols="40" class="form-control" placeholder="このセットについての説明(280文字以内)" maxlength="280" required id="id_description"></textarea>
+                  </p>
+                  <input type="hidden" name="folder_pk" value="58">
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                  <button type="submit" class="btn btn-danger" name="edit_folder">作成</button>
+                </div>
+                <!-- ./modal-footer -->
+              </form>
+            </div>
+            <!-- ./modal-content -->
+          </div>
+          <!-- ./modal-dialog -->
+        </div>
+        <!-- ./modal -->
+
+        <!-- 削除のモーダル設定 -->
+        <div class="modal fade" id="myDeleteModal58" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">セットの削除</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <form method="post">
+                <div class="modal-body">
+                  <input type="hidden" name="" value="">
+                  <p>(セット名)を削除してもよろしいでしょうか？</p>
+                  <input type="hidden" name="title" class="form-control" value="" placeholder="セットのタイトル" maxlength="50" required id="id_title">
+                  <input type="hidden" name="folder_pk" value="58">
+                </div>
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-danger form-control" name="delete_folder">削除</button>
+                </div>
+                <!-- ./modal-footer -->
+              </form>
+            </div>
+            <!-- ./modal-content -->
+          </div>
+          <!-- ./modal-dialog -->
+        </div>
+        <!-- ./modal -->
+      </div>
+    </div>
+
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">4</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  </div>
+
+  <!-- セット作成のモーダル設定 -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">セットを作成する</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <form method="post">
+          <div class="modal-body">
+            <input type="hidden" name="" value="">
+            <p>
+              <label for="id_file">セットの写真:</label>
+              <input type="file" name="image" value="test" size="50" id="id_file">
+            </p>
+            <p>
+              <label for="id_title">Title:</label>
+              <input type="text" name="title" class="form-control" placeholder="セットのタイトル" autofocus="autofocus" maxlength="50" id="id_title" required >
+            </p>
+            <p>
+              <label for="id_description">説明文:</label>
+              <textarea class="form-control" name="description" placeholder="セットの説明" autofocus="autofocus" maxlength="50" id="id_description" required ></textarea>
+            </p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+            <button type="submit" class="btn btn-danger" name="create_folder">作成</button>
+          </div>
+          <!-- ./modal-footer -->
+        </form>
+      </div>
+      <!-- ./modal-content -->
+    </div>
+    <!-- ./modal-dialog -->
+  </div>
+  <!-- ./modal -->
 
   <!-- Footer -->
   <footer>
