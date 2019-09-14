72186dc5342df3d19562fb6e0929ff762d5ff1e1
Katsuma-F <k.fujita.indigo1003@gmail.com>
2019-09-14 22:20:41 +0900

    fix indent

--- dbconnect.php
+++ dbconnect.php
@@ -2,6 +2,6 @@
 try {
   $db = new PDO('mysql:dbname=vinyl_bbs; host=localhost; charset=utf8', 'root', 'root');
 } catch(PDOException $e) {
-    print('DB接続エラー:' . $e->getMessage());
+  print('DB接続エラー:' . $e->getMessage());
 }
 ?>
