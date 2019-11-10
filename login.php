0908ada2aec241fc3f05d8e9c9857bba937d0424
Katsuma-F <k.fujita.indigo1003@gmail.com>
2019-11-10 16:17:23 +0900

    ->  in login.php

--- login.php
+++ login.php
@@ -3,11 +3,11 @@ session_start();
 require('./dbconnect.php');
 
 if ($_COOKIE['user_id'] !== '') {
-    $UserId = $_COOKIE['user_id'];
+    $UserID = $_COOKIE['user_id'];
 }
 
 if (!empty($_POST)) {
-    $UserId = $_POST['user_id'];
+    $UserID = $_POST['user_id'];
 
     if ($_POST['user_id'] !== '' && $_POST['password'] !== '') {
         $login = $db->prepare('SELECT * FROM users WHERE user_id=? AND password=?');
@@ -68,7 +68,7 @@ if (!empty($_POST)) {
       <form action="" method="post">
         <h1 class="mb-5">ログイン</h1>
         <div class="form-group">
-          <input class="form-control mb-4" name="user_id" type="text" placeholder="ユーザーid" value="<?php print(htmlspecialchars($UserId, ENT_QUOTES)); ?>">
+          <input class="form-control mb-4" name="user_id" type="text" placeholder="ユーザーid" value="<?php print(htmlspecialchars($UserID, ENT_QUOTES)); ?>">
           <?php if ($error['login'] === 'blank'): ?>
             <p class="error">*ユーザーidとパスワードをご記入ください</p>
           <?php endif; ?>
