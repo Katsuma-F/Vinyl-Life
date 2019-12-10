5368ed18cfc8aeb95548dceef25a7a9bcc9140be
Katsuma-F <k.fujita.indigo1003@gmail.com>
2019-12-10 21:58:57 +0900

    Rewrote commented out code for UserImgLoad.php

--- UserImgLoad.php
+++ UserImgLoad.php
@@ -1,7 +1,7 @@
 <?php
 require('./dbconnect.php');
 
-// 投稿画像を取得
+// プロフィール画像を取得
 $sql = 'SELECT image_type, image_content FROM users WHERE id = :id LIMIT 1';
 $images = $db->prepare($sql);
 $images->bindValue(':id', (int)$_GET['id'], PDO::PARAM_INT);
@@ -10,7 +10,7 @@ $image = $images->fetch();
 
 header('Content-type: ' . $image['image_type']);
 
-// 投稿画像を表示
+// プロフィール画像を表示
 echo $image['image_content'];
 exit();
 
